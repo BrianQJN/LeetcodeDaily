a0fe65c4f59c4c171abbed6b19d1e35f94ffd590
BrianQu <qujianning0401@163.com>
2024-08-16 16:30:34 -0400

    feature: LC217 Contains Duplicate

--- src/main/java/ArraysHashing/LC217_Contains_Duplicate.java
+++ src/main/java/ArraysHashing/LC217_Contains_Duplicate.java
@@ -1,5 +1,8 @@
 package ArraysHashing;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Brian Qu
  * @version 1.0
@@ -7,4 +10,19 @@ package ArraysHashing;
  * @since 2024-08-16
  */
 public class LC217_Contains_Duplicate {
+    public boolean containsDuplicate(int[] nums) {
+        // create a HashSet to store unique elements
+        Set<Integer> uniqueElements = new HashSet<>();
+
+        // iterate through the array
+        for (int num : nums) {
+            // add cur num to set, if it is seen, return true
+            if(!uniqueElements.add(num)) {
+                return true;
+            }
+        }
+
+        // no duplicates found, return false
+        return false;
+    }
 }
